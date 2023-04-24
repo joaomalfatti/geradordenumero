7c4a328722a659342cd7aadf94353dd984e4622f
joaomalfatti <joao.cursos1210@gmail.com>
2023-04-24 14:58:51 -0400

    Foi feita uma atualizzação onde consigo até salvar os números em um arquivo, é também, pedir o intervalo de tempo maior

--- geradornumber.py
+++ geradornumber.py
@@ -1,11 +1,31 @@
 import random
 
-# Pedir ao usuário para especificar o intervalo de números
-inicio = int(input("Digite o número inicial do intervalo: "))
-fim = int(input("Digite o número final do intervalo: "))
+# Perguntar ao usuário quantos números devem ser sorteados
+num_sorteados = int(input("Quantos números deseja sortear? "))
 
-# Gerar um número aleatório dentro do intervalo especificado
-numero_sorteado = random.randint(inicio, fim)
+# Perguntar ao usuário os limites do intervalo de sorteio
+limite_inf = int(input("Qual o valor mínimo do intervalo? "))
+limite_sup = int(input("Qual o valor máximo do intervalo? "))
 
-# Exibir o número sorteado
-print("O número sorteado foi:", numero_sorteado)
+# Sortear os números aleatórios
+sorteados = []
+for i in range(num_sorteados):
+    sorteados.append(random.randint(limite_inf, limite_sup))
+
+# Exibir os números sorteados
+print(f"Os {num_sorteados} números sorteados entre {limite_inf} e {limite_sup} foram:")
+for i in sorteados:
+    print(i)
+
+# Perguntar ao usuário se deseja salvar os números sorteados em um arquivo
+salvar_arquivo = input("Deseja salvar os números sorteados em um arquivo? (s/n) ").lower()
+if salvar_arquivo == "s":
+    nome_arquivo = input("Digite o nome do arquivo: ")
+    with open(nome_arquivo, "w") as arquivo:
+        for i in sorteados:
+            arquivo.write(str(i) + "\n")
+    print("Números sorteados salvos com sucesso!")
+elif salvar_arquivo == "n":
+    print("Os números sorteados não foram salvos em um arquivo.")
+else:
+    print("Opção inválida. Os números sorteados não foram salvos em um arquivo.")
